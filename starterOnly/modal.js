f1b5786087c731951c5799ce021fdb1ba4dd3bd2
popcornazerty <78792807+popcornazerty@users.noreply.github.com>
2021-09-27 16:59:30 +0200

    :construction: add modal confirmation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,6 +18,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  
 }
 
 // Fermer la modal
@@ -47,44 +48,91 @@ function submitModal() {
     inputFirstName.style.borderColor ='#ccc';
   }
 
-  // Nom ... à faire
-
+  // Nom 
+  const inputLastName = document.getElementById('last');
+  const errorLastName = document.getElementById('lastError');
+  
+  if (inputLastName.value.length < 2) {
+    errorLastName.innerText = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+    // Styles
+    errorLastName.style.color = 'red';
+    errorLastName.style.fontSize = '9px';
+    inputLastName.style.borderColor = 'red';
+  } else {
+    errorLastName.innerText = '';
+    inputLastName.style.borderColor ='#ccc';
+  }
   // Email
   const inputEmail = document.getElementById('email');
-  const errorEmail = document.getElementById('emailError');
+  const errorEmail = document.getElementById('errorEmail');
   //console.log(validateEmail('toto@titi.com'));
   //console.log(validateEmail('toto@'));
   //console.log(validateEmail('toto@t@iti.com'));
   
   if (validateEmail(inputEmail.value)) {
     errorEmail.innerText = '';
+    inputEmail.style.borderColor ='#ccc';
+    inputEmail.style.backgroundColor ='#fff';
     // CSS à faire
 
   } else {
-    errorEmail.innerText = 'Veuillez entrer une adresse email valide';
-    // CSS à faire
+    errorEmail.innerText = 'Veuillez entrer une adresse email valide.';
+    // Styles
+    errorEmail.style.color = 'red';
+    errorEmail.style.fontSize = '9px';
+    inputEmail.style.borderColor = 'red';
   }
 
   // Date de naissance
   const inputDate = document.getElementById('birthdate');
   const errorDate = document.getElementById('errorDate');
   if (validateDate(inputDate.value)){
-    errorDate.innerText = 'valide';
+    errorDate.innerText = '';
+    inputDate.style.borderColor =''
   } else {
-    errorDate.innerText = 'Vous devez entrer votre date de naissance au format DD/MM/YYYY';
+    errorDate.innerText = 'Merci de renseigner votre date de naissance';
+    // Styles
+    errorDate.style.color = 'red';
+    errorDate.style.fontSize ='9px';
+    inputDate.style.borderColor = 'red';
   }
 }
 
 document.getElementsByClassName('btn-submit')[0].onclick = function() {
   submitModal();
 }
-
+// regex validation email 
 function validateEmail(email) {
   const regex  = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
-
+//regex validation date de naissance
 function validateDate(date) {
   const regex = /^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$/;
   return regex.test(date);
 }
+// Vérifications de l'acceptation des termes et conditions
+
+/*
+const inputCheckbox = document.getElementById('checkbox1');
+const errorCheckbox = document.getElementById ('errorCheckbox');
+if (validateCheckbox(inputCheckbox.value.checked = true)){
+  
+}
+else{
+  errorCheckbox.innerText = 'Veuillez accepter les termes et conditions';
+}
+*/
+
+function validate() {
+  if (document.getElementById('checkbox1').checked) {
+      alert("checked");
+  } else {
+      alert("You didn't check it! Let me check it for you.");
+  }
+}
+
+
+
+
+
