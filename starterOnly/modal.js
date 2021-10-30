f33813ddf3c67d218ce83808853bca4521227a2d
popcornazerty <78792807+popcornazerty@users.noreply.github.com>
2021-10-30 12:58:02 +0200

    :construction: ajout verifications formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -49,6 +49,7 @@ function closeModal() {
 // vider autofill background des input
 
 //////////
+/*
 // Valider la modal
 function submitModal() {
   let validateForm = true;
@@ -66,150 +67,220 @@ function submitModal() {
     errorFirstName.innerText = '';
     inputFirstName.style.borderColor ='#ccc';
   }
+*/
+//TEST PRENOM
+
+/*
+const nameValue = document.getElementById("first").value;
+
+
+function validateForm(){
+/*
+    if( validateName(nameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.') === true &&  validateSurname(surnameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.') === true){
+    
+         // Affichage message formulaire valide
+    
+    }
+
+
+alert("test")
+validateName(nameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
+
+}
+
+
+const inputFirstName = document.getElementById('first');
+function validateName(first, message){
+
+   if(first === "" || first.length < 2){
+    
+    errorFirstName.style.color = 'red';
+    errorFirstName.style.fontSize = '9px';
+    inputFirstName.style.borderColor = 'red';
+       return message;
+        
+   }else{
+    inputFirstName.innerText = '';
+      inputFirstName.style.borderColor ='#ccc';
+       return true;
+       
+   
+   }
+
+}
+*/
 
 
 
-  // Nom 
-  const inputLastName = document.getElementById('last');
-  const errorLastName = document.getElementById('lastError');
-  
-  if (inputLastName.value.length < 2) {
-    errorLastName.innerText = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-    // Styles
-    errorLastName.style.color = 'red';
-    errorLastName.style.fontSize = '9px';
-    inputLastName.style.borderColor = 'red';
-    validateForm = false;
-  } else {
-    errorLastName.innerText = '';
-    inputLastName.style.borderColor ='#ccc';
-  }
-  // Email
+document.getElementById('btn-sub').addEventListener('click',function(){
+//Prénom
+  const prenom = document.getElementById("firstName");
+  const nom = document.getElementById("lastName");
+  const errorFirstName = document.getElementById("errorFirstName");
+  const errorLastName = document.getElementById("errorLastName");
   const inputEmail = document.getElementById('email');
   const errorEmail = document.getElementById('errorEmail');
-  //console.log(validateEmail('toto@titi.com'));
-  //console.log(validateEmail('toto@'));
-  //console.log(validateEmail('toto@t@iti.com'));
-  
-  if (validateEmail(inputEmail.value)) {
-    errorEmail.innerText = '';
-    inputEmail.style.borderColor ='#ccc';
-    inputEmail.style.backgroundColor ='#fff';
-    
-
-  } else {
-    errorEmail.innerText = 'Veuillez entrer une adresse email valide.';
-    // Styles
-    errorEmail.style.color = 'red';
-    errorEmail.style.fontSize = '9px';
-    inputEmail.style.borderColor = 'red';
-    validateForm = false;
-  }
-
-  // Date de naissance
   const inputDate = document.getElementById('birthdate');
   const errorDate = document.getElementById('errorDate');
-  if (validateDate(inputDate.value)){
-    errorDate.innerText = '';
-    inputDate.style.borderColor =''
+  const inputQuantity = document.getElementById('quantity');
+  const errorQuantity = document.getElementById('errorQuantity');
+
+  const ville = document.getElementsByClassName("city");
+  const errorCity = document.getElementById("errorCity");
+
+  if(
+    validateName(prenom, errorFirstName, "Merci de renseigner un prenom supérieur à deux caractères") === true
+    && validateName(nom, errorLastName, "Merci de renseigner un nom supérieur à deux caractères") === true
+    && validateEmail(inputEmail, errorEmail, 'Veuillez entrer une adresse email valide.')
+    && validateBirth(inputDate, errorDate, 'Merci de renseigner votre date de naissance')
+    && validateQuantity(inputQuantity, errorQuantity, 'Merci de renseigner des chiffres uniquement')
+    && validateCity(ville, errorCity, 'Merci de selectionner une ville et de renseigner le nombre de tournois')
+    ) {
+    // Formulaire valide
+    // Ferme la modale inscription 
+    const closeModal = document.getElementById('bground1');
+    closeModal.style.display = 'none';
+    // Ouvre la modale confirmation
+    openModalSuccess();
+
   } else {
-    errorDate.innerText = 'Merci de renseigner votre date de naissance';
-    // Styles
-    errorDate.style.color = 'red';
-    errorDate.style.fontSize ='9px';
-    inputDate.style.borderColor = 'red';
-    validateForm = false;
+    // Erreur dans le formulaire
+    alert('Merci de remplir l\'intégralité du formulaire');
   }
- 
 
-// Verification qu'au moins une ville est cochée si le nombre de tournois est > 1
-/*
-const nombreDeTournois = document.getElementById('quantity');
-let isValid = false;
-if (nombreDeTournois.value > 0) {
- const checkboxInput = document.getElementsByClassName('city');
- for (let checkbox of checkboxInput) {
-   if (checkbox.checked) {
-     isValid = true;
-   }
- }
-}
+});
 
-if (isValid) {// quantité > 0 ET au moins une ville de cocher
-  // Pas d'erreur (formulaire valide)
-  document.getElementById('btn-sub').disabled = false;
-} else {
-  // Erreur
-  alert('Merci de selectionner une ville et de renseigner le nombre de tournois');
-  document.getElementById('btn-sub').disabled = true;
-}
-*/
-
-
-
-
-// verification valeurs numériques uniquement
-//
-
-const inputQuantity = document.getElementById('quantity');
-const errorQuantity = document.getElementById('errorQuantity');
-if (validateNumber(inputQuantity.value)){
-  errorQuantity.innerText = '';
-  inputQuantity.style.borderColor =''
-} else {
-  errorQuantity.innerText = 'Merci de renseigner des chiffres uniquement';
-  // Styles
-  errorQuantity.style.color = 'red';
-  errorQuantity.style.fontSize ='9px';
-  inputQuantity.style.borderColor = 'red';
-  validateForm = false;
+function validateName(name, domNode, errorMessage) {
+  if( name.value === "" || name.value.length < 2) {
+      // Lire l'api classList
+      name.style.border = "3px solid red";
+      domNode.classList.add("errorMessage");
+      domNode.innerText = errorMessage;
+      return false;
+    }  else {
+      name.style.border = 'none';
+      domNode.classList.remove("errorMessage");
+      domNode.innerText = '';
+      return true;
+    } 
 }
 
 
-//regex validation quantité
-function validateNumber(quantity) {
-    
-    alert(Number.isInteger(quantity))
 
+
+
+// Email
+
+
+
+function validateEmail(email, domNode, errorMessage) {
+  if(regexEmail(email.value) == false) {
+      // Lire l'api classList
+      email.style.border = "3px solid red";
+      domNode.classList.add("errorMessage");
+      domNode.innerText = errorMessage;
+      return false;
+   
+   } else {
+      email.style.border = 'none';
+      domNode.classList.remove("errorMessage");
+      domNode.innerText = '';
+      return true;
+
+   } 
 }
- //OR
-/*
- function validateNumber(quantity) {
-  const regex = /^[\s\d]*$/;
-  return regex.test(quantity);
-  }
-*/
-
 
 // regex validation email 
-function validateEmail(email) {
+function regexEmail(email) {
   const regex  = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
+
+
+//date de naissance
 //regex validation date de naissance
 function validateDate(date) {
   const regex = /^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$/;
   return regex.test(date);
 }
-  // Vérifications de l'acceptation des termes et conditions
-  // changement couleur texte rouge si non acceptation conditions 
-  const erreurUtilisation = document.getElementById("conditionUtilisation");
-  if (document.getElementById('checkbox1').checked == false) {
-    alert("Merci ce cocher la case \"J'ai lu et accepté les conditions d'utilisation.\"");
-    erreurUtilisation.style.color = 'red';
-    validateForm = false;
-  } 
-  if(validateForm) {
-    // redirection
-    closeModal();
-    openModalSuccess();
-    closeModalSuccess(); 
-    document.getElementById('form-inscription').reset(); 
+
+function validateBirth(date, domNode, errorMessage) {
+  if(validateDate(date.value) === false) {
+    date.style.border = "3px solid red";
+    domNode.classList.add("errorMessage");
+    domNode.innerText = errorMessage;
+    return false;
+ } else {
+      date.style.border = 'none';
+      domNode.classList.remove("errorMessage");
+      domNode.innerText = '';
+      return true;
+ } 
+}
+
+//verification villes 
+/*
+let isValid = false;
+
+if (isValid) {
+  document.getElementById('btn-sub').disabled = false;
+} else {
+  document.getElementById('btn-sub').disabled = true;
+  errorclick.innerText = 'Merci de cocher au moins une ville';
+  errorclick.style.color = 'red';
+  errorclick.style.fontSize ='9px';
+  //alert('Merci de selectionner une ville et de renseigner le nombre de tournois');
+}
+*/
+
+// verification valeurs numériques uniquement
+//
+
+
+function validateQuantity(quantity, domNode, errorMessage) {
+  quantity.value = parseInt(quantity.value);
+  if (quantity.value > 0) {
+    console.log("if");
+  quantity.style.border = 'none';
+  domNode.classList.remove("errorMessage");
+  domNode.innerText = '';
+  return true; }
+  else {
+
+    console.log("else");
+    quantity.style.border = "3px solid red";
+    domNode.classList.add("errorMessage");
+    domNode.innerText = errorMessage;
+    return false;
+  }
+}
+
+function validateCity(city, domNode, errorMessage) {
+  let isValid = false;
+  for (let c of city) {
+    if (c.checked) {
+      isValid = true;
+    }
   }
 
+  if(isValid) {
+    
+    domNode.classList.remove("errorMessage");
+    domNode.innerText = '';
+  } else {
+    
+    domNode.classList.add("errorMessage");
+    domNode.innerText = errorMessage;
+  }
+
+  return isValid;
 }
 
 
+
+
+
+////////
 function openModalSuccess() {
   const modalSuccess = document.getElementById('modal-success');
   modalSuccess.style.display = "block";
@@ -217,7 +288,7 @@ function openModalSuccess() {
   
 }
 
-function closeModalSuccess() {
+/*function closeModalSuccess() {
   const modalSuccessBtn = document.getElementById('modal-success-close');
   modalSuccessBtn.onclick = function() {
     const modalSuccess = document.getElementById('modal-success');
@@ -226,9 +297,5 @@ function closeModalSuccess() {
     document.body.style.oberflow = 'visible';
 
   }
-}
+}*/
 
-
-document.querySelector(".btn-submit").onclick = function() {
-  submitModal();
-};
